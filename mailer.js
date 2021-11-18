67cf7c0ed49ffb2add7c20e6f3a3cce6fe8fd1c2
qige2016 <286882998@qq.com>
2021-11-18 15:52:24 +0800

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -4,19 +4,17 @@ const mailer = (to, key) => {
   const transporter = nodemailer.createTransport({
     service: "QQ",
     auth: {
-        user: '286882998@qq.com',
-        pass: 'iphgtizkxnbmbiif'
+        user: '',
+        pass: ''
     }
   })
 
   const receiver = {
-      from: `"发码机器人"<286882998@qq.com>`,
-      subject: '金庸群侠传3D重制Steam激活码',
+      from: ``,
+      subject: '',
       to: to,
       html: 
-      `<p>您的Steam激活码为：${key}，请谨慎保管！</p>
-       <p>激活步骤：Steam左下角“添加游戏”-在Steam上激活产品-输入key激活即可！</p>
-      `
+      ``
   }
 
   return new Promise((resolve, reject) => {
